856499964e073a21e9d19b3d45d0e14ad2b86647
Kurellanaveen1498 <naveenkurella28@gmail.com>
2024-11-11 17:54:04 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,28 +1,22 @@
-def decode_message( s: str, p: str) -> bool:
-
-
-   
-    dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
-   
+def is_match(message: str, key: str) -> bool:
+    m = len(message)
+    k = len(key)
+    
+    dp = [[False] * (k + 1) for _ in range(m + 1)]
+    
     dp[0][0] = True
-
-
-    for j in range(1, len(p) + 1):
-        if p[j - 1] == '*':
-            dp[0][j] = dp[0][j - 1]
-
-  
-    for i in range(1, len(s) + 1):
-        for j in range(1, len(p) + 1):
-            if p[j - 1] == '*':
-              
-                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
-            elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
-               
+    
+    for j in range(1, k + 1):
+        if key[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1] 
+    
+    for i in range(1, m + 1):
+        for j in range(1, k + 1):
+            if key[j - 1] == '?':
                 dp[i][j] = dp[i - 1][j - 1]
-
-   
-    return dp[len(s)][len(p)]
-
-  
-    return False
+            elif key[j - 1] == '*':
+                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i - 1][j - 1] and message[i - 1] == key[j - 1]
+    
+    return dp[m][k]
