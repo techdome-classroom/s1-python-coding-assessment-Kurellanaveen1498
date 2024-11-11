779449bd1ce08cf68d1e583fc9e65d65930382ef
Kurellanaveen1498 <naveenkurella28@gmail.com>
2024-11-11 17:58:33 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,29 +1,28 @@
-def is_match(message: str, key: str) -> bool:
-    m = len(message)
-    k = len(key)
-    
-    dp = [[False] * (k + 1) for _ in range(m + 1)]
-    
+def decode_message( s: str, p: str) -> bool:
+
+
+   
+    dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+   
     dp[0][0] = True
-    
-    for j in range(1, k + 1):
-        if key[j - 1] == '*':
-            dp[0][j] = dp[0][j - 1] 
-    
-    for i in range(1, m + 1):
-        for j in range(1, k + 1):
-            if key[j - 1] == '?':
-                dp[i][j] = dp[i - 1][j - 1]
-            elif key[j - 1] == '*':
+
+
+    for j in range(1, len(p) + 1):
+        if p[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+
+  
+    for i in range(1, len(s) + 1):
+        for j in range(1, len(p) + 1):
+            if p[j - 1] == '*':
+              
                 dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
-            else:
-                dp[i][j] = dp[i - 1][j - 1] and message[i - 1] == key[j - 1]
+            elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
+               
+                dp[i][j] = dp[i - 1][j - 1]
 
-    return dp[m][k]
+   
+    return dp[len(s)][len(p)]
 
-print(is_match("aa", "a"))      # False
-print(is_match("aa", "*"))      # True
-print(is_match("cb", "?a"))     # False
-print(is_match("abc", "a*"))    # True
-print(is_match("abc", "*b"))    # True
-print(is_match("abc", "abc?"))  # True
+  
+    return False
